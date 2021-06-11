d55ed695416852916b7c1f6d98a222c95a7b8458
Robin Munn <rmunn@pobox.com>
2021-06-11 17:04:03 +0700

    Initial version of Auth0 wrapper code
    This code wraps the @auth0/nextjs-auth0 library since that's the one
    whose API is closest to the Svelte-Kit way of doing things.

---
+++ src/auth.ts
@@ -0,0 +1,97 @@
+import { initAuth0 as origInitAuth0 } from '@auth0/nextjs-auth0';
+import { auth0Wrapper, auth0WrapperJson } from './wrappers';
+import type { Request as SvelteRequest, RequestHandler, Load, LoadInput, LoadOutput } from '@sveltejs/kit';
+
+export function initAuth0(config) {
+  let auth0 = origInitAuth0(config);
+  let getSession = auth0WrapperJson((req, res) => auth0.getSession(req, res));
+  const loginUrl = config?.routes?.login || '/api/auth/login';
+  return {
+    getSession: getSession,
+    getAccessToken: auth0WrapperJson((req, res, auth0FnOptions) => auth0.getAccessToken(req, res, auth0FnOptions)),
+    handleLogin: auth0Wrapper((req, res, auth0FnOptions) => auth0.handleLogin(req, res, auth0FnOptions)),
+    handleLogout: auth0Wrapper((req, res, auth0FnOptions) => auth0.handleLogout(req, res, auth0FnOptions)),
+    handleCallback: auth0Wrapper((req, res, auth0FnOptions) => auth0.handleCallback(req, res, auth0FnOptions)),
+    handleProfile: auth0Wrapper((req, res, auth0FnOptions) => auth0.handleProfile(req, res, auth0FnOptions)),
+
+    handleAuth(svelteReq: SvelteRequest) {
+      const param = svelteReq.params.auth0;
+      const route = Array.isArray(param) ? param[0] : param;
+      switch (route) {
+        case 'login':
+          return this.handleLogin(svelteReq);
+        case 'logout':
+          return this.handleLogout(svelteReq);
+        case 'callback':
+          return this.handleCallback(svelteReq);
+        case 'me':
+          return this.handleProfile(svelteReq);
+        default:
+          return;  // Fall through to other handlers
+      };
+    },
+
+    withPageAuthRequired(opts?: { load?: Load, returnTo? : string }) {
+      return (loadParams: LoadInput) => {
+        const isAuthenticated = loadParams.session?.isAuthenticated;
+        if (isAuthenticated) {
+          const user = loadParams.session?.user;
+          if (opts?.load && typeof opts?.load === 'function') {
+            var loadResult = opts?.load(loadParams);
+            // Handle either promises or non-promises without making this function async
+            if (loadResult && typeof (loadResult as any).then === 'function') {
+              // Async load() function
+              return (loadResult as Promise<LoadOutput>).then(loadResult => {
+                if (loadResult) {
+                  return { ...loadResult, props: { ...loadResult.props, user, isAuthenticated }}
+                } else {
+                  // If user's load() function returned nothing, they intend to fall through, so we shouldn't populate user props
+                  return loadResult;
+                }
+              })
+            } else if (loadResult) {
+              // Synchronous load()
+              return { ...loadResult, props: { ...(loadResult as LoadOutput).props, user, isAuthenticated }}
+            } else {
+              // If user's load() function returned nothing, they intend to fall through, so we shouldn't populate user props
+              return loadResult;
+            }
+          } else {
+            // No load function passed, so just populate user prop
+            return { props: { user, isAuthenticated }}
+          }
+        } else {
+          const returnUrl = opts?.returnTo || `${loadParams.page.path}?${loadParams.page.query.toString()}`;
+          return {
+            status: 307,
+            redirect: `${loginUrl}?returnTo=${encodeURIComponent(returnUrl)}`
+          }
+        }
+      }
+    },
+
+    withApiAuthRequired: (route: RequestHandler, unauthHandler: RequestHandler, auth0FnOptions?: any): RequestHandler => {
+      return (svelteReq) => {
+        const session = getSession(svelteReq);
+        if (session && session.user) {
+          svelteReq.locals.user = {
+            ...svelteReq.locals.user,
+            ...session.user
+          };
+          svelteReq.locals.isAuthenticated = true;
+          return route(svelteReq);
+        } else if (unauthHandler && typeof unauthHandler === "function") {
+          return unauthHandler(svelteReq);
+        } else {
+          return {
+            status: 401,
+            body: {
+              error: 'not_authenticated',
+              description: 'The user does not have an active session or is not authenticated'
+            }
+          };
+        }
+      };
+    },
+  }
+}
---
+++ src/wrappers.ts
@@ -0,0 +1,143 @@
+import url from 'url';
+import type { Request, EndpointOutput } from '@sveltejs/kit';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import type { ReadOnlyFormData, Headers } from '@sveltejs/kit/types/helper';
+
+function ensureLeadingSlash(path: string) {
+    return path.startsWith('/') ? path : `/${path}`;
+}
+
+function buildUrl(host: any, path: any, query: URLSearchParams) {
+    const slashPath = ensureLeadingSlash(path || '/');
+    let urlObj = new url.URL(`https://${host}${slashPath}`);
+    if (typeof query === "string") {
+        urlObj.search = query;
+    } else if (query && typeof query.toString === "function") {
+        urlObj.search = query.toString();
+    }
+    return urlObj.toString();
+}
+
+// TODO: Implement caching with WeakMap so that auth0's session cache can be used to best effect, as otherwise we'll be calling this too often
+// Cache should, given the same Svelte request object, always return the same NextApiRequest mimic
+function mkReq(param: Request) : NextApiRequest {
+    const result : any = {
+        method: 'GET',
+        headers: (param.headers ? param.headers : param) as Headers,
+        query: Object.fromEntries(param.query as any),  // TODO: Check whether just plain `param.query` will work here
+        url: buildUrl(param.host || 'localhost', param.path || '/', param.query),
+        // TODO: Build "cookies" object since Auth0 will want it
+    };
+    if (param.body && typeof (param.body as ReadOnlyFormData).getAll === "function") {
+        // Body is a ReadOnlyFormData object from Svelte, but auth0-nextjs will expect a plain object
+        result.body = Object.fromEntries(param.body as any);
+    } else if (typeof param.body === "string") {
+        result.body = param.body;
+    } else {
+        result.body = param.body || {};
+    }
+    return result;
+}
+
+// TODO: Implement caching with WeakMap based on original Svelte request object (which we'll preserve a reference to in the ResMimic instance so that they're deferenced together)
+class ResMimic {
+    headers: Map<any, any>;
+    statusCode: number;
+    statusMessage: string | undefined;
+    bodyObj: { [key: string]: any };
+    bodyStr: string;
+
+    constructor() {
+        this.headers = new Map();
+        this.statusCode = 200;
+        this.bodyObj = undefined;
+        this.bodyStr = "";
+    }
+
+    getHeader(key: any) {
+        const value = this.headers.get(key);
+        if (value && value.length === 1) {
+            return value[0];
+        } else {
+            return value;
+        }
+    }
+
+    setHeader(key: string, value: any) {
+        const oldValue = this.headers.get(key.toLowerCase()) || [];
+        const newValue = typeof value === 'string' ? [...oldValue, value] : [...oldValue, ...value];
+        this.headers.set(key.toLowerCase(), newValue);
+        return this;
+    }
+
+    writeHead(status: number, reason?: any, headers?: any) {
+        this.statusCode = status;
+        let realHeaders;
+        if (typeof reason === 'string') {
+            this.statusMessage = reason;
+            realHeaders = headers;
+        } else if (typeof reason === 'object' && !headers) {
+            realHeaders = reason;
+        } else {
+            realHeaders = headers || {};
+        }
+        for (const key in realHeaders) {
+            if (realHeaders.hasOwnProperty(key)) {
+                this.setHeader(key, realHeaders[key]);
+            }
+        }
+        return this;
+    }
+
+    end() {
+        return this;
+    }
+
+    send(body: { [key: string]: any } | string) {
+        if (typeof body === "object") {
+            this.bodyObj = body;
+        } else {
+            this.bodyStr = body;
+        }
+        return this;
+    }
+
+    status(statusCode: any) {
+        this.statusCode = statusCode;
+        return this;
+    }
+
+    json(bodyObj: any) {
+        this.bodyObj = bodyObj;
+        this.setHeader('content-type', 'application/json');
+        return this;
+    }
+
+    getSvelteResponse() : EndpointOutput {
+        const status = this.statusCode;
+        const headers = {};
+        for (const [k, v] of this.headers.entries()) {
+            headers[k] = (v.length === 1 ? v[0] : v);
+        }
+        const body = this.bodyObj ? this.bodyObj : this.bodyStr;
+        return { status, headers, body };
+    }
+}
+
+function auth0Wrapper(auth0fn: (req: NextApiRequest, res: NextApiResponse, arg2?: any) => Promise<any>) {
+    return (param: any, auth0FnOptions: any) => {
+        const req = mkReq(param);
+        const res = new ResMimic();
+        return auth0fn(req, (res as unknown) as NextApiResponse, auth0FnOptions).then(() => { return res.getSvelteResponse(); }).catch((error: any) => ({ status: 500, body: error }));
+    };
+}
+
+function auth0WrapperJson(auth0fn: (req: NextApiRequest, res: NextApiResponse, arg2?: any) => any) {
+    return (svelteReq: Request, auth0FnOptions?: any) => {
+        const req = mkReq(svelteReq);
+        const res = new ResMimic();
+        return auth0fn(req, (res as unknown) as NextApiResponse, auth0FnOptions);
+    };
+}
+
+export { mkReq, ResMimic, auth0Wrapper, auth0WrapperJson }
